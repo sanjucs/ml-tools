81166ef2cf2e7f3f2fe930b47fd636b403d41235
sanjucs <csanju.s@gmail.com>
2025-03-15 15:56:00 +0530

    WER

--- tools/metrics/wer.py
+++ tools/metrics/wer.py
@@ -1,5 +1,3 @@
-import copy
-
 SUB = 1
 INS = 1
 DEL = 1
@@ -10,7 +8,7 @@ def argmin(a):
 def get_cost(x):
   return x[0] * SUB + x[1] * INS + x[2] * DEL
 
-def set_cost(dp, r, c, prev):
+def set_counts(dp, r, c, prev):
   for i in range(4):
     dp[r][c][i] = prev[i]
 
@@ -36,16 +34,16 @@ def compute_wer(target, predicted, is_case_sensitive=False, debug=False):
   for i in range(1, target_len + 1):
     for j in range(1, predicted_len + 1):
       if target[i-1] == predicted[j - 1]:
-        dp[i][j][3] = copy.copy(dp[i - 1][j - 1][3]) + 1
+        set_counts(dp, i, j, dp[i - 1][j - 1])
       else:
-        prev_costs = [dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]]
+        prev_counts = [dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]]
         add_costs = [SUB, DEL, INS]
-        cost_sub = get_cost(prev_costs[0]) + add_costs[0]
-        cost_del = get_cost(prev_costs[1]) + add_costs[1]
-        cost_ins = get_cost(prev_costs[2]) + add_costs[2]
+        cost_sub = get_cost(prev_counts[0]) + add_costs[0]
+        cost_del = get_cost(prev_counts[1]) + add_costs[1]
+        cost_ins = get_cost(prev_counts[2]) + add_costs[2]
         index = argmin([cost_sub, cost_del, cost_ins])
 
-        set_cost(dp, i, j, prev_costs[index])
+        set_counts(dp, i, j, prev_counts[index])
         dp[i][j][index] += add_costs[index]
 
   final = dp[-1][-1]
