bc6fb37c6c4c809dd59fdcfa1c61761f33e2f9c9
sanjucs <csanju.s@gmail.com>
2025-03-16 14:55:34 +0530

    WER implementation

--- tools/metrics/wer.py
+++ tools/metrics/wer.py
@@ -1,15 +1,16 @@
 class WER:
-  def __init__(self, SUB=1, INC=1, DEL=1):
+  def __init__(self, SUB=1, INS=1, DEL=1, COR=0):
     self.SUB = SUB
-    self.INC = INC
+    self.INS = INS
     self.DEL = DEL
+    self.COR = COR
 
   @staticmethod
   def argmin(a):
       return min(range(len(a)), key=lambda x : a[x])
 
   def get_cost(self, x):
-    return x[0] * self.SUB + x[1] * self.INC + x[2] * self.DEL
+    return x[0] * self.SUB + x[1] * self.INS + x[2] * self.DEL + x[3] * self.COR
 
   @staticmethod
   def set_counts(dp, r, c, prev):
@@ -28,6 +29,7 @@ class WER:
     target_len = len(target)
 
     dp = [[[0] * 4 for _ in range(predicted_len + 1)] for _ in range(target_len + 1)]
+    add_costs = [self.SUB, self.DEL, self.INS]
 
     for i in range(1, predicted_len + 1):
       dp[0][i][1] = i
@@ -42,35 +44,29 @@ class WER:
           dp[i][j][3] += 1
         else:
           prev_counts = [dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]]
-          add_costs = [self.SUB, self.DEL, self.INC]
           cost_sub = self.get_cost(prev_counts[0]) + add_costs[0]
           cost_del = self.get_cost(prev_counts[1]) + add_costs[1]
-          cost_INC = self.get_cost(prev_counts[2]) + add_costs[2]
-          index = self.argmin([cost_sub, cost_del, cost_INC])
+          cost_INS = self.get_cost(prev_counts[2]) + add_costs[2]
+          index = self.argmin([cost_sub, cost_del, cost_INS])
 
           self.set_counts(dp, i, j, prev_counts[index])
           dp[i][j][index] += add_costs[index]
 
     return dp[-1][-1]
 
-  def cost(self, target, predicted, is_case_sensitive=False):
+  def compute(self, target, predicted, is_case_sensitive=False):
     x = self._compute(target, predicted, is_case_sensitive)
-    return sum(x[:-1]) / sum(x)
-    
-  def SDIC(self, target, predicted, is_case_sensitive=False):
-    x = self._compute(target, predicted, is_case_sensitive)
-    return {'S': x[0], 
+    cost = sum(x[:-1]) / sum(x)
+    SDIC = {'S': x[0], 
             'D': x[1],
             'I': x[2],
             'C': x[3],
-            }
+          }
+    return {'cost': cost, 'SDIC': SDIC}
 
 if __name__ == '__main__':
   target = "The guard arrived late because it was raining"
   predicted = "The guard arrived late because of the rain"
 
-  wer = WER().cost(target, predicted)
-  print("score::", wer)
-
-  SDIC = WER().SDIC(target, predicted)
-  print("SDIC::", SDIC)
+  wer = WER().compute(target, predicted)
+  print(wer)
